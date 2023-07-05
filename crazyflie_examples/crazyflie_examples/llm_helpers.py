58aa35533aeb9e446fb4e1d6baf6567eb828d85b
Sanjmi Khurana <sanjmi.khurana@mail.utoronto.ca>
2023-07-05 16:22:44 +0200

    return string at the end

--- crazyflie_examples/crazyflie_examples/llm_helpers.py
+++ crazyflie_examples/crazyflie_examples/llm_helpers.py
@@ -258,7 +258,7 @@ Make sure the drones don't touch the ground (their z coordinate should always be
             self.all_waypoints[:, i+2] /= (np.abs(self.all_waypoints[:, i+2]).max() / self.limits[key]) if self.all_waypoints[:, i+2].max() != 0 and self.all_waypoints[:, i+2].any() > self.limits[key] else 1
 
         self.all_waypoints_list = self.all_waypoints.tolist()
-        return self.all_waypoints_list
+        return str(self.all_waypoints_list)
 
 def main():
     pass
